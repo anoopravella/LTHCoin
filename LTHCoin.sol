eb76db96b02705e20296bca7e6f95396c486d99c
anoopravella <34986736+anoopravella@users.noreply.github.com>
2018-06-14 11:24:17 +0200

    Update LTHCoin.sol

--- LTHCoin.sol
+++ LTHCoin.sol
@@ -92,9 +92,7 @@ contract LTHCoin is StandardToken {
     string public name;                   // Token Name
     uint8 public decimals;                // How many decimals to show. To be standard complicant keep it 18
     string public symbol;                 // An identifier: eg SBX, XPR etc..
-    string public version = 'H1.0'; 
     uint256 public unitsOneEthCanBuy;     // How many units of your coin can be bought by 1 ETH?
-    uint256 public totalEthInWei;         // WEI is the smallest unit of ETH (the equivalent of cent in USD or satoshi in BTC). We'll store the total ETH raised via our ICO here.  
     address public fundsWallet;           // Collect all the ether we receive
 
     // This is a constructor function 
